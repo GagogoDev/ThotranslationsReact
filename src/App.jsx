e864d28ccee7cce0d50fb305839bf49b1db38891
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2024-05-12 17:18:17 -0400

    release(base_proyecto):  - Personalización tema bootstrap  - Cambios importación bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
     return (
         <>
-            <h1>Thotranslation</h1>
+            <div className="row mt-5">
+                <div className="col-12 text-center">
+                    <h1 className="text-primary">Thotranslation</h1>
+                    <h2 className="text-secondary">En construcción</h2>
+                </div>
+            </div>
         </>
     );
 }
