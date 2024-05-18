e85642c6893efe637250c81ed7626f0f3cf21d11
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2024-05-18 16:46:46 -0400

    main:  - Implementación i18next  - Cambio de idioma dinámico es/en

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,63 @@
-import React from 'react';
-import SpanishIcon from '/icons/language/Spanish-icon.png';
+// React
+import React, { useState } from 'react';
+
+// Images
 import LogoSimple from '/icons/Logo-simple.png';
 
+// Translations
+import { useTranslation } from 'react-i18next';
+
 export const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#"><img src={ LogoSimple } alt="Imagen" className="navbar-icon" /></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarContent">
-                <ul className="navbar-nav me-auto mb-1 mb-lg-0">
-                    <li className="nav-item px-1">
-                        <a className="nav-link fw-bold text-primary" href="#">INICIO</a>
-                    </li>
-                    <li className="nav-item px-1">
-                        <a className="nav-link fw-bold text-primary" href="#">NOSOTROS</a>
-                    </li>
-                    <li className="nav-item px-1">
-                        <a className="nav-link fw-bold text-primary" href="#">SERVICIOS</a>
-                    </li>
-                    <li className="nav-item px-1">
-                        <a className="nav-link fw-bold text-primary" href="#">PROCESO</a>
-                    </li>
-                    <li className="nav-item px-1">
-                        <a className="nav-link fw-bold text-primary" href="#">CONTACTO</a>
-                    </li>
-                </ul>
-                <form className="d-flex">
-                    <button className="btn fw-bold text-primary p-0 d-flex align-items-center" type="button">
-                        <img src={ SpanishIcon } alt="Imagen" className="language-icon" /> ESPAÑOL
-                    </button>
-                </form>
+    const [ language, setlanguage ] = useState( 'es' );
+    const [ t, i18n ] = useTranslation( "global" );
+
+    const onChangeLanguage = () => {
+        switch (language) {
+            case 'es':
+                i18n.changeLanguage('en');
+                setlanguage('en');
+                break;
+            case 'en':
+                i18n.changeLanguage('es');
+                setlanguage('es');
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><img src={ LogoSimple } alt="Imagen" className="navbar-icon" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarContent">
+                    <ul className="navbar-nav me-auto mb-1 mb-lg-0">
+                        <li className="nav-item px-1">
+                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.home" ) }</a>
+                        </li>
+                        <li className="nav-item px-1">
+                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.about_us" ) }</a>
+                        </li>
+                        <li className="nav-item px-1">
+                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.services" ) }</a>
+                        </li>
+                        <li className="nav-item px-1">
+                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.process" ) }</a>
+                        </li>
+                        <li className="nav-item px-1">
+                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.contact" ) }</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <button className="btn fw-bold text-primary p-0 d-flex align-items-center" type="button" onClick={onChangeLanguage}>
+                            <img src={ t( "navbar.test" ) } alt="Imagen" className="language-icon" /> { t( "navbar.language" ) }
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
+// Styles
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+// Translations i18n
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+import translation_es from './assets/translations/es/global.json';
+import translation_en from './assets/translations/en/global.json';
+
+i18next.init({
+    interpolation: { scapeValue: false },
+    lng: "es",
+    resources: {
+        es: { global: translation_es },
+        en: { global: translation_en },
+    },
+});
+
+// ReactDOM
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
     </React.StrictMode>
 );
