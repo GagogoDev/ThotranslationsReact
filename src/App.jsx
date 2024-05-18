14c82b302b8daa75347054786414d14236b013fa
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2024-05-18 17:19:35 -0400

    main:  - Importar navbar y styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+// Components
+import { Navbar } from './components/Navbar';
+
+// Styles
+import './assets/css/styles.css';
+
 function App() {
     return (
         <>
-            <div className="row mt-5">
+            <Navbar />
+            <div className="row m-0 mt-5">
                 <div className="col-12 text-center">
                     <h1 className="text-primary">Thotranslation</h1>
                     <h2 className="text-secondary">En construcción</h2>
