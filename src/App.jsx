075f7d131f6b80d86ceb3abece72fbfec5dbefa0
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2024-08-14 21:23:35 -0400

    React Scroll Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -41,22 +41,22 @@ function App() {
                     <img src={LogoCompleto} alt="LogoCompleto" className='logo-thotranslations animate__animated animate__fadeInUp' />
                 </div>
             </div>
-            <div className="row d-flex justify-content-center bg-primary text-light m-0">
+            <div className="row d-flex justify-content-center bg-primary text-light m-0" id="section-nosotros">
                 <div className="col-11 col-md-10 align-items-center pt-5">
                     <Nosotros />
                 </div>
             </div>
-            <div className="row d-flex justify-content-center bg-light text-primary m-0">
+            <div className="row d-flex justify-content-center bg-light text-primary m-0" id="section-servicios">
                 <div className="col-11 col-md-10 align-items-center pt-5">
                     <Servicios />
                 </div>
             </div>
-            <div className="row d-flex justify-content-center bg-primary text-light m-0">
+            <div className="row d-flex justify-content-center bg-primary text-light m-0" id="section-proceso">
                 <div className="col-11 col-md-10 align-items-center pt-5">
                     <Proceso />
                 </div>
             </div>
-            <div className="row d-flex justify-content-center bg-light text-primary m-0">
+            <div className="row d-flex justify-content-center bg-light text-primary m-0" id="section-contacto">
                 <div className="col-11 col-md-10 align-items-center pt-5">
                     <Contacto />
                 </div>
--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,5 +1,5 @@
 // React
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Images
 import LogoSimple from '/icons/Logo-simple.png';
@@ -10,10 +10,13 @@ import '../../assets/css/navbar.css';
 // Translations
 import { useTranslation } from 'react-i18next';
 
+// React Scroll
+import { Link } from 'react-scroll';
+
 export const Navbar = () => {
     // Translations
-    const [ language, setlanguage ] = useState( 'es' );
-    const [ t, i18n ] = useTranslation( "global" );
+    const [language, setlanguage] = useState('es');
+    const [t, i18n] = useTranslation('global');
 
     const onChangeLanguage = () => {
         switch (language) {
@@ -28,40 +31,108 @@ export const Navbar = () => {
             default:
                 break;
         }
-    }
-    
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src={ LogoSimple } alt="Imagen" className="navbar-icon" /></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="#">
+                    <img
+                        src={LogoSimple}
+                        alt="Imagen"
+                        className="navbar-icon"
+                    />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarContent"
+                    aria-controls="navbarContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarContent">
                     <ul className="navbar-nav me-auto mb-1 mb-lg-0">
                         <li className="nav-item px-1">
-                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.inicio" ) }</a>
+                            <a
+                                className="nav-link fw-bold text-primary"
+                                href="#"
+                            >
+                                {t('navbar.inicio')}
+                            </a>
                         </li>
-                        <li className="nav-item px-1">
-                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.nosotros" ) }</a>
-                        </li>
-                        <li className="nav-item px-1">
-                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.servicios" ) }</a>
-                        </li>
-                        <li className="nav-item px-1">
-                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.proceso" ) }</a>
-                        </li>
-                        <li className="nav-item px-1">
-                            <a className="nav-link fw-bold text-primary" href="#">{ t( "navbar.contacto" ) }</a>
-                        </li>
+
+                        <Link
+                            className="nav-item px-1"
+                            to="section-nosotros"
+                            offset={-60}
+                        >
+                            <a
+                                className="nav-link fw-bold text-primary"
+                                href="#"
+                            >
+                                {t('navbar.nosotros')}
+                            </a>
+                        </Link>
+
+                        <Link
+                            className="nav-item px-1"
+                            to="section-servicios"
+                            offset={-60}
+                        >
+                            <a
+                                className="nav-link fw-bold text-primary"
+                                href="#"
+                            >
+                                {t('navbar.servicios')}
+                            </a>
+                        </Link>
+
+                        <Link
+                            className="nav-item px-1"
+                            to="section-proceso"
+                            offset={-60}
+                        >
+                            <a
+                                className="nav-link fw-bold text-primary"
+                                href="#"
+                            >
+                                {t('navbar.proceso')}
+                            </a>
+                        </Link>
+
+                        <Link
+                            className="nav-item px-1"
+                            to="section-contacto"
+                            offset={-60}
+                        >
+                            <a
+                                className="nav-link fw-bold text-primary"
+                                href="#"
+                            >
+                                {t('navbar.contacto')}
+                            </a>
+                        </Link>
                     </ul>
                     <form className="d-flex">
-                        <button className="language-button btn fw-bold text-primary p-0 d-flex align-items-center" type="button" onClick={onChangeLanguage}>
-                            <img src={ t( "navbar.lenguaje_imagen" ) } alt="Imagen" className="language-icon" /> { t( "navbar.lenguaje" ) }
+                        <button
+                            className="language-button btn fw-bold text-primary p-0 d-flex align-items-center"
+                            type="button"
+                            onClick={onChangeLanguage}
+                        >
+                            <img
+                                src={t('navbar.lenguaje_imagen')}
+                                alt="Imagen"
+                                className="language-icon"
+                            />{' '}
+                            {t('navbar.lenguaje')}
                         </button>
                     </form>
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
